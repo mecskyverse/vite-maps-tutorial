de291bf34238c70117c2854dc3a830061625dba9
Aakash <vermaaakash1000@gmail.com>
2023-05-14 09:42:11 +0530

    code arrangements

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 // Importing some Hooks and components from google maps api which we will be using during this tutorial
 import { useJsApiLoader, GoogleMap, Marker, Autocomplete, DirectionsRenderer } from '@react-google-maps/api'
 
 
 const libraries = ['places'];
 function App() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
+
+  // We will be using some state variables to store the response from the google maps api
   const [directionsResponse, setDirectionsResponse] = useState(null)
   const [distance, setDistance] = useState('')
   const [duration, setDuration] = useState('')
-  const originRef = React.useRef()
-  const destinationRef = React.useRef()
+
+  /** @type React.MutableRefObject<HTMLInputElement> */
+  const sourceRef = useRef()
+  /** @type React.MutableRefObject<HTMLInputElement> */
+  const destinationRef = useRef()
+
+  // We Have to select some coordinates to give to the map to initially show the map from that point.
+  // This are the latitude and longitude of New Delhi, India
+  const center = { lat: 28.7041, lng: 77.1025 }
 
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API,
@@ -19,35 +26,17 @@ function App() {
   })
 
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log('Name:', name);
-    console.log('Email:', email);
-    // Perform submit logic here, such as sending the data to a server
-  };
 
-  const handleNameChange = (event) => {
-    setName(event.target.value);
-  };
 
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value);
-  };
-
-  console.log(isLoaded)
   // function to calculate Route
-  async function calculateRoute(data) {
-    console.log(data)
-    data.origin = originRef.current.value
-    data.destination = destinationRef.current.value
-    console.log(data)
-    console.log(originRef.current.value)
-    if (originRef.current.value === '' && destinationRef.current.value === '') {
+  async function calculateRoute() {
+    event.preventDefault();
+    if (sourceRef.current.value === '' || destinationRef.current.value === '') {
       return
     }
     const directionsService = new google.maps.DirectionsService()
     const results = await directionsService.route({
-      origin: originRef.current.value,
+      origin: sourceRef.current.value,
       destination: destinationRef.current.value,
       travelMode: google.maps.TravelMode.DRIVING
     })
@@ -55,9 +44,14 @@ function App() {
     setDistance(results.routes[0].legs[0].distance.text)
     setDuration(results.routes[0].legs[0].duration.text)
   }
-  // We Have to select some coordinates to give to the map to initially show the map from that point.
-  // This are the latitude and longitude of New Delhi, India
-  const center = { lat: 28.7041, lng: 77.1025 }
+
+  function clearRoute() {
+    setDirectionsResponse(null)
+    setDistance('')
+    setDuration('')
+    sourceRef.current.value = ''
+    destinationRef.current.value = ''
+  }
 
 
   if (!isLoaded)
@@ -67,19 +61,31 @@ function App() {
   return (
     <>
       <main className='container'>
-        <form className='container-form' onSubmit={handleSubmit} >
-          <Autocomplete >
-            <input type="text" value={name} onChange={handleNameChange} placeholder='Enter Source' />
-          </Autocomplete >
+        <div className='sub-container'>
+          <form className='container-form' onSubmit={calculateRoute} >
+            <Autocomplete >
+              <input type="text" placeholder='Enter Source' ref={sourceRef} />
+            </Autocomplete >
 
-          <br />
-          <Autocomplete >
-            <input type="email" value={email} onChange={handleEmailChange} placeholder='Enter Destination' />
-          </Autocomplete >
-          <br />
-          <button type="submit">Submit</button>
-        </form>
+            <br />
+            <Autocomplete >
+              <input type="text" placeholder='Enter Destination' ref={destinationRef} />
+            </Autocomplete >
+            <br />
+            <button type='submit'>Submit</button>
+          </form>
 
+          <button className='self-center' onClick={clearRoute}>Clear Route</button>
+
+          {
+            directionsResponse && (
+              <div className='self-center'>
+                <div>Distance = {distance}</div>
+                <div>Duration= {duration}</div>
+              </div>
+            )
+          }
+        </div>
         <div style={{ width: "50vw", height: '100vh' }}>
           <GoogleMap
             center={center}
@@ -93,8 +99,8 @@ function App() {
             }}
           >
             {console.log(directionsResponse)}
-            {directionsResponse && <DirectionsRenderer directions={directionsResponse} />}
             <Marker position={center} />
+            {directionsResponse && <DirectionsRenderer directions={directionsResponse} />}
           </GoogleMap>
         </div>
 
